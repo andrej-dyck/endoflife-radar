37ea467a500d321fcbfe197847428db439ca19e3
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2024-03-15 08:42:54 +0100

    add clear button to search box

--- src/ui-components/SearchBox.tsx
+++ src/ui-components/SearchBox.tsx
@@ -32,12 +32,15 @@ export const SearchBox = ({ label, placeholder, formClassName, onChange, onFocus
           onBlur={() => onFocusChange?.(false)}
           onKeyDown={onEsc(() => value ? changeValue('') : ref.current?.blur())}
         />
-        <div
-          className="absolute end-0 top-0 flex h-full flex-col place-content-center rounded-e-lg border border-primary-element bg-primary-element p-2 transition-all focus:ring-4  focus:ring-focus"
+        <button
+          className="absolute end-0 top-0 flex h-full flex-col place-content-center rounded-e-lg border border-primary-element bg-primary-element p-2 transition-all focus:ring-4 focus:ring-focus"
+          disabled={!value}
+          onClick={() => changeValue('')}
         >
-          <SearchIcon className="size-5" />
-          <span className="sr-only">Search</span>
-        </div>
+          {!value
+            ? <SearchIcon className="size-5" />
+            : <><CancelIcon className="size-5" /><span className="sr-only">clear</span></>}
+        </button>
       </div>
     </div>
   </form>
@@ -48,5 +51,13 @@ const SearchIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" aria-hidden="true" viewBox="0 0 24 24" {...props}>
     <path fill="currentColor"
       d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5q0-1.875-1.312-3.187T9.5 5Q7.625 5 6.313 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14"></path>
+  </svg>
+)
+
+/** material-symbols:close-rounded */
+const CancelIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <path fill="currentColor"
+      d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"></path>
   </svg>
 )
