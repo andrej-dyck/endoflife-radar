744ec4bdf14c7095e5f7d489aaf275dc52f94413
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2025-10-17 10:05:17 +0200

    update dependencies

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import en from './en.json' assert { type: 'json ' }
+import en from './en.json' with { type: 'json ' }
 
 void i18n
   .use(initReactI18next)
