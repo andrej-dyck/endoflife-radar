b3e555e7f7ef1d141187cad1df7ba72dfd83eb01
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2025-10-18 12:04:35 +0200

    split asset chunks

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,16 @@ export default defineConfig({
     react({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => {
+          if (id.includes('react-router')) return '@react-router'
+          if (id.includes('react')) return '@react'
+          if (id.includes('zod')) return '@zod'
+          return undefined
+        },
+      },
+    },
+  },
 })
